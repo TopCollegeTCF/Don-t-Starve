02f80cdebbb38aa763038f18da8f966dd1412b6c
muesimka <docudiksjeje@gmail.com>
2026-03-11 12:19:10 +0300

    Update assets.js

--- js/assets.js
+++ js/assets.js
@@ -1,1 +1,20 @@
-
+window.helloGraphics = function () {
+    hello();
+    bye();
+}
+function hello() {
+    const img = document.getElementById('testImage');
+    if (img.complete) {
+        console.log("Assets & Graphics ready 🖼️ (изображение загружено)");
+    } else {
+        console.log("Assets & Graphics ready 🖼️");
+    }
+}
+function bye() {
+    const img = document.getElementById('testImage');
+    if (img.complete) {
+        console.log("Assets & Graphics ready 🖼️ (изображение загружено)");
+    } else {
+        console.log("Assets & Graphics ready 🖼️");
+    }
+}
