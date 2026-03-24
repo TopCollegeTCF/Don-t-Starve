9efe67a92300f85a4182ede5849696e0e0e1719e
kpe4 <anafievtimur007@gmail.com>
2026-03-24 13:30:12 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -3,3 +3,36 @@ function helloUI() {
 }
 
 helloUI();
+// Функция для рисования полоски здоровья
+window.drawHealthBar = function(ctx, x, y, healthPercent) {
+    const barWidth = 200;
+    const barHeight = 20;
+    const fillWidth = (healthPercent / 100) * barWidth;
+    
+    // TODO: Нарисовать красный прямоугольник шириной fillWidth
+    // TODO: Нарисовать чёрную обводку вокруг всей полоски
+    // 👇 Твой код здесь
+    ctx.fillStyle = 'red';
+    ctx.fillRect(x, y, fillWidth, barHeight);
+    
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(x, y, barWidth, barHeight);
+}
+
+// Функция для рисования полоски голода
+window.drawHungerBar = function(ctx, x, y, hungerPercent) {
+    const barWidth = 200;
+    const barHeight = 20;
+    const fillWidth = (hungerPercent / 100) * barWidth;
+    
+    // TODO: Нарисовать зелёный прямоугольник шириной fillWidth
+    // TODO: Нарисовать чёрную обводку вокруг всей полоски
+    // 👇 Твой код здесь
+    ctx.fillStyle = 'green';
+    ctx.fillRect(x, y, fillWidth, barHeight);
+    
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(x, y, barWidth, barHeight);
+}
+
+drawHungerBar();
