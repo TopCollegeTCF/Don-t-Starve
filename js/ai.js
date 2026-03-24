b63be3762981fdee6d7544e93d59aeb50b2a9a3c
requisite <hzharetrolit@gmail.com>
2026-03-23 22:31:05 -1200

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -11,3 +11,10 @@ function hello() {
 function bye() {
     console.log("AI ВСЕ!");
 }
+window.drawSpider = function(ctx, x, y) {
+
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0, Math.PI * 2);
+    ctx.fill();
+}
