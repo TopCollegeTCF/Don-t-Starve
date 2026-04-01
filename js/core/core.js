1b727d2579ae64e01580a93776ce6d65eab96db3
Artijetsu <buhankacopyright@gmail.com>
2026-04-01 12:15:56 +0300

    Update core.js

--- js/core/core.js
+++ js/core/core.js
@@ -11,7 +11,7 @@ window.drawPlayerBody = function(ctx, x, y) {
 
 }
 
- develop
+
 // Функция для игрового цикла (заглушка)
 window.startGameLoop = function() {
     console.log("🎮 Game loop initialized");
